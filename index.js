a79e04d6d5c5f3d235c75781a15cc6ab133f556c
Joe Tustin <j.tustin@gmail.com>
2019-02-16 20:27:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
- /*
-function destructivelyAppendKitten {
+
+function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
 
+/*
 function destructivelyAppendKitten(name) {
   var newKittens = [...kittens, name];
   return newKittens;
